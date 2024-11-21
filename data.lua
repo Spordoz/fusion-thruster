8b6f3c181a147b970d0352a0bfbcbd9eb18013ac
drhaber <Drhaber@live.com>
2024-11-21 10:17:09 -0500

    Flavor Text and Technology

--- data.lua
+++ data.lua
@@ -126,3 +126,52 @@ thrusterfusionreactor.surface_conditions =
 },
 
 data:extend{fusionthruster, fusionthruster_item, fusionthruster_recipe, thrusterfusionreactor, thrusterfusionreactor_item, thrusterfusionreactor_recipe}
+
+data:extend(
+{
+	{
+		type = "technology",
+		name = "fusion-thruster",
+		icons = 
+		{
+			{
+				icon = "__core__/graphics/empty.png",
+			},
+			{
+				icon = "__fusion-thruster__/graphics/fusion-thruster-reasearch.png",
+				icon_size = 64,
+				scale = 1.5
+			}
+		},
+		effects =
+		{
+			{
+				type = "unlock-recipe",
+				recipe = "fusion-thruster"
+			},
+			{
+				type = "unlock-recipe",
+				recipe = "thruster-fusion-reactor"
+			}      			
+		},
+		prerequisites = {"fusion-reactor"},
+		unit =
+		{
+			count = 2000,
+			ingredients =
+			{
+        {"automation-science-pack", 1},
+        {"logistic-science-pack", 1},
+        {"chemical-science-pack", 1},
+        {"production-science-pack", 1},
+        {"utility-science-pack", 1},
+        {"space-science-pack", 1},
+        {"metallurgic-science-pack", 1},
+        {"agricultural-science-pack", 1},
+        {"electromagnetic-science-pack", 1},
+        {"cryogenic-science-pack", 1}
+			},
+			time = 60
+	}
+  }  
+})
